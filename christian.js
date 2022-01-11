18dbb5c19349299f813e26d535f1b15149e1ed00
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-11 11:40:08 +0100

    Christian

---
+++ christian.js
@@ -0,0 +1,13 @@
+const euro = [12.5, 30, 2.75, 80, 17, 0];
+const dollar = [];
+
+function convert(betrag) {
+  // toFixed(2) kürzt das Ergebnis auf 2 Stellen nach dem Komma
+  // und gibt einen String zurück
+  let zwischenSchritt = (betrag * 1.1334).toFixed(2);
+  // Number() wandelt den String wieder in eine Zahl um
+  dollar.push(Number(zwischenSchritt));
+}
+
+euro.map(convert);
+console.log(dollar);
