ef509a78d26c1edbf6c27ed95d9ea9888233ccb1
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-11 13:38:04 +0100

    Lösung

--- christian.js
+++ christian.js
@@ -4,10 +4,11 @@ const dollar = [];
 function convert(betrag) {
   // toFixed(2) kürzt das Ergebnis auf 2 Stellen nach dem Komma
   // und gibt einen String zurück
-  let zwischenSchritt = (betrag * 1.1334).toFixed(2);
+  let zwischenSchritt = (betrag * 1.14).toFixed(2);
   // Number() wandelt den String wieder in eine Zahl um
   dollar.push(Number(zwischenSchritt));
 }
 
 euro.map(convert);
-console.log(dollar);
+console.log("Euro-Beträge  :", euro);
+console.log("... in Dollar :", dollar);
